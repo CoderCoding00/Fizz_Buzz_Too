8cfec87bb3c6bd84df80e65c54716aed319ba650
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2023-01-21 22:50:34 -0800

    updated file format

--- main.cpp
+++ main.cpp
@@ -18,13 +18,14 @@ string fizzBuzzToo(int n)
 {
     string result;
 
-    if(n % 3 == 0 && n % 5 == 0)
+    if (n % 3 == 0 && n % 5 == 0)
         cout << "FizzBuzz" << endl;
-    else if(n % 3 == 0)
-        cout << "Fizz" <<endl;
+    else if (n % 3 == 0)
+        cout << "Fizz" << endl;
     else if (n % 5 == 0)
         cout << "Buzz" << endl;
-    else cout << n << '!' << endl;
+    else
+        cout << n << '!' << endl;
 
     return result;
 }
